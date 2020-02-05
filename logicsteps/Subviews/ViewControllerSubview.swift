67acc42553c9ac90ca9491377b453f3c888f1bd1
kenphanith <phanith.ken@quad.co.jp>
2020-02-05 21:30:35 +0900

    wip: ui

--- logicsteps/Subviews/ViewControllerSubview.swift
+++ logicsteps/Subviews/ViewControllerSubview.swift
@@ -12,6 +12,38 @@ import SnapKit
 
 final class ViewControllerSubview {
     
+    // board
+    lazy var board: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.layer.cornerRadius = 15
+        return view
+    }()
+    
+    // level
+    lazy var level: UILabel = {
+        let label = UILabel()
+        label.text = "Level 1"
+        label.textColor = .gray
+        return label
+    }()
+    
+    // instruction
+    lazy var instruction: UILabel = {
+        let label = UILabel()
+        label.text = "Paint the fruit one by one with the given instruction and color \n 1.Fill/paint the orange with orange color.\n Expected output:"
+        return label
+    }()
+    
+    // point
+    lazy var point: UILabel = {
+        let label = UILabel()
+        label.text = "point 0"
+        label.textAlignment = .right
+        label.textColor = .yellow
+        return label
+    }()
+    
     // red button
     lazy var redBtn: ColorButton = {
         let btn = ColorButton(color: UIColor.red, label: "Red")
@@ -61,11 +93,87 @@ final class ViewControllerSubview {
     }()
     
     func updateSubviewConstraints(viewController: ViewController){
+        
+        self.board.snp.makeConstraints{ make in
+            make.width.equalToSuperview().dividedBy(1.2)
+            make.height.equalTo(60)
+            make.top.equalToSuperview().offset(20)
+            make.centerX.equalToSuperview()
+        }
+        
+        self.level.snp.makeConstraints{ make in
+            make.width.equalToSuperview().dividedBy(2)
+            make.height.equalToSuperview()
+            make.left.equalToSuperview().offset(15)
+        }
+        
+        self.point.snp.makeConstraints{ make in
+            make.width.equalToSuperview().dividedBy(2)
+            make.height.equalToSuperview()
+            make.right.equalToSuperview().offset(-15)
+        }
+        
         self.redBtn.snp.makeConstraints{ make in
-            make.width.equalToSuperview().dividedBy(6)
+            make.width.equalToSuperview().dividedBy(4)
             make.height.equalTo(40)
-            make.top.equalToSuperview()
-            make.
+            make.top.equalTo(self.board.snp.bottom).offset(15)
+            make.left.equalTo(self.board.snp.left)
+        }
+        
+        self.blueBtn.snp.makeConstraints{ make in
+            make.width.equalToSuperview().dividedBy(4)
+            make.height.equalTo(40)
+            make.top.equalTo(self.board.snp.bottom).offset(15)
+            make.left.equalTo(self.redBtn.snp.right).offset(15)
+        }
+        
+        self.greenBtn.snp.makeConstraints{ make in
+            make.width.equalToSuperview().dividedBy(4)
+            make.height.equalTo(40)
+            make.top.equalTo(self.board.snp.bottom).offset(15)
+            make.left.equalTo(self.blueBtn.snp.right).offset(15)
+        }
+        
+        self.orangeBtn.snp.makeConstraints{ make in
+            make.width.equalToSuperview().dividedBy(4)
+            make.height.equalTo(40)
+            make.top.equalTo(self.redBtn.snp.bottom).offset(15)
+            make.left.equalTo(self.board.snp.left)
+        }
+        
+        self.grayBtn.snp.makeConstraints{ make in
+            make.width.equalToSuperview().dividedBy(4)
+            make.height.equalTo(40)
+            make.top.equalTo(self.redBtn.snp.bottom).offset(15)
+            make.left.equalTo(self.orangeBtn.snp.right).offset(15)
+        }
+        
+        self.pinkBtn.snp.makeConstraints{ make in
+            make.width.equalToSuperview().dividedBy(4)
+            make.height.equalTo(40)
+            make.top.equalTo(self.redBtn.snp.bottom).offset(15)
+            make.left.equalTo(self.grayBtn.snp.right).offset(15)
+        }
+        
+        self.yellowBtn.snp.makeConstraints{ make in
+            make.width.equalToSuperview().dividedBy(4)
+            make.height.equalTo(40)
+            make.top.equalTo(self.orangeBtn.snp.bottom).offset(15)
+            make.left.equalTo(self.board.snp.left)
+        }
+        
+        self.purpleBtn.snp.makeConstraints{ make in
+            make.width.equalToSuperview().dividedBy(4)
+            make.height.equalTo(40)
+            make.top.equalTo(self.orangeBtn.snp.bottom).offset(15)
+            make.left.equalTo(self.yellowBtn.snp.right).offset(15)
+        }
+        
+        self.instruction.snp.makeConstraints{ make in
+            make.width.equalToSuperview().dividedBy(1.1)
+            make.height.equalToSuperview().dividedBy(2.5)
+            make.centerX.equalToSuperview()
+            make.top.equalTo(self.purpleBtn.snp.bottom).offset(15)
         }
     }
 }
--- logicsteps/ViewController.swift
+++ logicsteps/ViewController.swift
@@ -8,14 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+    
+    private let subviews: ViewControllerSubview = ViewControllerSubview()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor.cyan
         
-        let redBtn: ColorButton = ColorButton(color: UIColor.red, label: "Red")
-        self.view.addSubview(redBtn)
+        self.subviews.board.addSubview(self.subviews.level)
+        self.subviews.board.addSubview(self.subviews.point)
+        self.view.addSubview(self.subviews.board)
+        
+        self.view.addSubview(self.subviews.redBtn)
+        self.view.addSubview(self.subviews.blueBtn)
+        self.view.addSubview(self.subviews.greenBtn)
+        self.view.addSubview(self.subviews.orangeBtn)
+        self.view.addSubview(self.subviews.grayBtn)
+        self.view.addSubview(self.subviews.pinkBtn)
+        self.view.addSubview(self.subviews.yellowBtn)
+        self.view.addSubview(self.subviews.purpleBtn)
+        
+        self.view.addSubview(self.subviews.instruction)
     }
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        self.subviews.updateSubviewConstraints(viewController: self)
+    }
 }
 
